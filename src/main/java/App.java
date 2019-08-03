85ac1b8038c0bbb1da03125b4df35729165655e1
Lixard <51443500+Lixard@users.noreply.github.com>
2019-08-04 01:40:20 +0300

    day one ended

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,13 +5,12 @@ import java.sql.Statement;
 
 public class App {
 
-    private static final String URL = "jdbc:postgresql://127.0.0.1:5432/MailRuHW";
+    private static final String URL = "jdbc:postgresql://127.0.0.1:5432/MailRuHW4";
     private static final String USERNAME = "postgres";
     private static final String PASSWORD = "123";
 
     public static void main(String[] args) {
-        try (Connection connection = DriverManager.getConnection(URL,USERNAME,PASSWORD))
-        {
+        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
             Statement statement = connection.createStatement();
             statement.close();
         } catch (SQLException e) {
